2e5c242c069c39783da34971c1d81c457e974d70
Levi Imbuzan <Imbuzanlevi@yahoo.com>
2025-01-12 22:38:37 -0500

    Updating notes/making code changes

---
+++ Module 2 - Data Types and Variables/Galactic Travel Agency/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class GalacticTravelAgency { 
+  public static void Main(string[] args) {
+    // Your code goes here
+
+    string passengerName = "Zara";
+    int passengerAge = 28 + 1 ;
+    string ticketType = "First Class";
+    string preferredPlanet = "Mars";
+
+    Console.WriteLine(passengerName + '\n' + passengerAge + '\n' + ticketType + '\n' + preferredPlanet);
+
+    double passengerAgeDouble = (double)passengerAge;
+    Console.WriteLine(passengerAgeDouble);
+
+    double passengerAgeDouble2 = (passengerAge);
+    Console.WriteLine(passengerAgeDouble2);
+
+    string passengerAgeString= Convert.ToString(passengerAge);
+    Console.WriteLine(passengerAgeString);
+  }
+}
